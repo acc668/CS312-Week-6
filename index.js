b2f1df6e249ae007567abdec5b902448c6efc3a2
acc668 <116805246+acc668@users.noreply.github.com>
2024-07-17 21:35:17 -0700

    exercise 2 files committed

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const tesla = { model: "Model S", topSpeed: 200, topColour: "Red" };
+const honda = { model: "Civic", topSpeed: 150, topColour: "Blue" };
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+ReactDOM.render(
+  <table>
+    <thead>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{tesla.topSpeed}</td>
+        <td>{tesla.topColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{honda.topSpeed}</td>
+        <td>{honda.topColour}</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById("root")
+);
